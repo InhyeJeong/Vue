0502b5616f2e8884fe73ddd7b1fa6ce8a396dba8
InhyeJeong <okoooc@hanmail.net>
2019-03-25 17:17:55 +0900

    검색결과 구현

--- Vue.JS-06.Inflean/mine/2-vue/js/app.js
+++ Vue.JS-06.Inflean/mine/2-vue/js/app.js
@@ -1,21 +1,34 @@
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
     el: '#app',
     data: {
         //  입력 data를 받아서 저장
-        query: ''
+        query: '',
+        searchResult: [],
+        submitted: false
     },
     methods: {
         onSubmit: function(e) {
-            
+            this.search()
         },
-        onKeyup: function() {
-            if(!this.query.length) this.onReset()
+        onKeyup: function(e) {
+            if(!this.query.length) this.resetForm()
         },
-        onReset: function() {
+        onReset: function(e) {
+            this.resetForm()
+        },
+        search: function () {
+            SearchModel.list().then(data => {
+                this.submitted = true
+                this.searchResult = data
+            })
+        },
+        resetForm: function() {
             //  여기에서 this는 Vue instance
             this.query = '';
             //  todo 검색결과를 숨기는 ...
-            debugger
+
         },
         
     },
