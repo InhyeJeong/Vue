9f65e8d58ade3a64ca0a3368803c30b1d42076a0
InhyeJeong <okoooc@hanmail.net>
2019-03-25 16:44:19 +0900

    검색폼
    1. v-model : 양방향 바인딩
    2. v-on:submit="onSubmit" 이벤트 리스너
    3.  v-on:submit.prevent="onSubmit" 기본동작 막기

--- Vue.JS-06.Inflean/mine/2-vue/js/app.js
+++ Vue.JS-06.Inflean/mine/2-vue/js/app.js
@@ -1,6 +1,12 @@
 new Vue({
     el: '#app',
     data: {
-        msg:'hello world'
-    }
+        //  입력 data를 받아서 저장
+        query: ''
+    },
+    methods: {
+        onSubmit: function(e) {
+            debugger
+        }
+    },
 })
