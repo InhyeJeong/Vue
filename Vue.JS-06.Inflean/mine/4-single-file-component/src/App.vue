00d146f7ae8a0f2a3d67510dd7ad15e71aa0076b
InhyeJeong <okoooc@hanmail.net>
2019-03-27 15:51:38 +0900

    단일컴포넌트(FormComponent)

--- Vue.JS-06.Inflean/mine/4-single-file-component/src/App.vue
+++ Vue.JS-06.Inflean/mine/4-single-file-component/src/App.vue
@@ -3,17 +3,81 @@
     <header>
       <h2 class="container">검색</h2>
     </header>
+    <div class="container">
+      <search-form v-bind:value="query" v-on:@submit="onSubmit" v-on:@reset="onReset"></search-form>
+    </div>
   </div>
 </template>
 
 <script>
+import SearchModel from './models/SearchModel.js'
+import KeyWordModel from './models/KeyWordModel.js'
+import HistoryModel from './models/HistoryModel.js'
+
+import FormComponent from './components/FormComponent.vue'
+
 export default {
   name: 'app',
   data () {
     return {
-
+      query: '',
+      searchResult: [],
+      keywords: [],
+      history: [],
+      submitted: false,
+      tabs: ['추천 검색어', '최근 검색어'],
+      selectedTab: '',
     }
-  }
+  },
+  components: {
+    'search-form' : FormComponent
+  },
+  methods: {
+    onSubmit: function(query) {
+            this.query = query
+            this.search()
+    },
+    onReset: function(e) {
+        this.resetForm()
+    },
+    //  검색될때마다 호출됨
+    search: function () {
+        SearchModel.list().then(data => {
+            this.submitted = true
+            this.searchResult = data
+        })
+        HistoryModel.add(this.query)
+        this.fetchHistory()
+    },
+    resetForm: function() {
+        //  여기에서 this는 Vue instance
+        this.query = ''
+        //  todo 검색결과를 숨기는 ...
+        this.submitted = false
+        this.searchResult = []
+    },
+    onClickTab: function(tab) {
+        this.selectedTab = tab
+    },
+    onClickKeyword: function (keyword) {
+        this.query = keyword
+        this.search()
+    },
+    fetchKeyword: function () {
+        KeyWordModel.list().then(data => {
+            this.keywords = data
+        })
+    },
+    fetchHistory: function () {
+        HistoryModel.list().then(data => {
+            this.history = data
+        })
+    },
+    onclickRemoveHistory: function (keyword) {
+        HistoryModel.remove(keyword)
+        this.fetchHistory()
+    }
+  },
 }
 </script>
 
---
+++ Vue.JS-06.Inflean/mine/4-single-file-component/src/components/FormComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <form v-on:submit.prevent="onSubmit">
+    <input type="text" v-model="inputValue" v-on:keyup="onKeyup" placeholder="검색어를 입력하세요" autofocus>
+    <button v-show="inputValue.length" v-on:click="onReset" type="reset" class="btn-reset"></button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  data() {
+      return {
+        inputValue: this.value
+      }
+  },
+  methods: {
+      onSubmit: function () {
+        //  child -> parent
+        this.$emit('@submit', this.inputValue.trim())
+      },
+      onKeyup: function () {
+        if(!this.inputValue.length) this.onReset()
+      },
+      onReset: function () {
+        this.inputValue = ''
+        this.$emit('@reset')
+      }
+  },
+}
+</script>
+
+<style>
+
+</style>
